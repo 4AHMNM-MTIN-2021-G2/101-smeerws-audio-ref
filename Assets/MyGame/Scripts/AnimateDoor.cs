6c765ba40e8ef482ea06203b606bc57dc2011b3e
smeerws <hello@smeerws.at>
2021-06-19 09:42:49 +0200

    add animaition enctrance

---
+++ Assets/MyGame/Scripts/AnimateDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateDoor : MonoBehaviour
+{
+
+    private Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    public void StartOpenDoorAnimation()
+    {
+        myAnimator.SetBool("openDoor", true);
+        //myGameData.isDoorOpened = true;
+    }
+}
