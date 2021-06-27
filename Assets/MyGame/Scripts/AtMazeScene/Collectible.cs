9da8d9a9fff359d104e563e60e019d67b780de94
smeerws <hello@smeerws.at>
2021-06-27 12:31:08 +0200

    maze with gameplay

---
+++ Assets/MyGame/Scripts/AtMazeScene/Collectible.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public string collectibleColor;
+    public bool isClollected;
+    public GameData gameData;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        gameData.AddCollectible(collectibleColor);
+        Destroy(gameObject);
+    }
+}
